e2b9eb900e015722c99725e84f47b18cf2fa56e7
lwh <2391519868@qq.com>
2018-09-28 16:26:41 +0800

    Netty学习

--- src/main/java/com/lwh/netty/chp6/NioDemo1.java
+++ src/main/java/com/lwh/netty/chp6/NioDemo1.java
@@ -1,5 +1,11 @@
 package com.lwh.netty.chp6;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+
 /**
  * @author lwh
  * @date 2018-09-28
@@ -7,7 +13,24 @@ package com.lwh.netty.chp6;
  */
 public class NioDemo1 {
 
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        int[] ports = new int[5];
+
+        ports[0] = 5000;
+        ports[1] = 5001;
+        ports[2] = 5002;
+        ports[3] = 5003;
+        ports[4] = 5004;
+
+        Selector selector = Selector.open();
+
+        for(int i = 0; i < ports.length; i++){
+            ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+            //设置非阻塞
+            serverSocketChannel.configureBlocking(false);
+            ServerSocket serverSocket = serverSocketChannel.socket();
+            InetSocketAddress address = new InetSocketAddress(ports[i]);
+            serverSocket.bind(address);
+        }
     }
 }
