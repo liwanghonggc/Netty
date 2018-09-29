eddc920a3b50c7d39e33a9f103c2cfb2f4e86485
lwh <2391519868@qq.com>
2018-09-29 08:25:57 +0800

    Netty学习

--- src/main/java/com/lwh/netty/chp6/NioDemo1.java
+++ src/main/java/com/lwh/netty/chp6/NioDemo1.java
@@ -3,8 +3,9 @@ package com.lwh.netty.chp6;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
-import java.nio.channels.Selector;
-import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.*;
+import java.util.Iterator;
+import java.util.Set;
 
 /**
  * @author lwh
@@ -31,6 +32,36 @@ public class NioDemo1 {
             ServerSocket serverSocket = serverSocketChannel.socket();
             InetSocketAddress address = new InetSocketAddress(ports[i]);
             serverSocket.bind(address);
+
+            //注册,对什么感兴趣
+            serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+            System.out.println("监听端口: " + ports[i]);
+        }
+
+        while (true){
+            int num = selector.select();
+            System.out.println("num: " + num);
+
+            Set<SelectionKey> selectionKeys = selector.selectedKeys();
+            System.out.println("selectionKeys: " + selectionKeys);
+
+            Iterator<SelectionKey> itr = selectionKeys.iterator();
+
+            while (itr.hasNext()){
+                SelectionKey selectionKey = itr.next();
+                if(selectionKey.isAcceptable()){
+                    ServerSocketChannel serverSocketChannel = (ServerSocketChannel) selectionKey.channel();
+                    SocketChannel socketChannel = serverSocketChannel.accept();
+                    socketChannel.configureBlocking(false);
+
+                    socketChannel.register(selector, SelectionKey.OP_READ);
+
+                    //一定要移除
+                    itr.remove();
+
+                    System.out.println("获得客户端的连接: " + socketChannel);
+                }
+            }
         }
     }
 }
