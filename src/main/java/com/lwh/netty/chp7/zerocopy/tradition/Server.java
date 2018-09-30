bd36ff2663bd10d6ae691827b26fe487ba118c9a
lwh <2391519868@qq.com>
2018-09-30 08:29:55 +0800

    Netty学习

--- src/main/java/com/lwh/netty/chp7/zerocopy/tradition/Server.java
+++ src/main/java/com/lwh/netty/chp7/zerocopy/tradition/Server.java
@@ -1,12 +1,14 @@
 package com.lwh.netty.chp7.zerocopy.tradition;
 
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 /**
  * @author lwh
  * @date 2018-09-29
- * @desp
+ * @desp 零拷贝例子对比
  */
 public class Server {
 
@@ -14,7 +16,21 @@ public class Server {
         ServerSocket serverSocket = new ServerSocket(8899);
 
         while (true){
+            Socket socket = serverSocket.accept();
+            DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
 
+            try{
+                byte[] byteArray = new byte[4096];
+                while(true){
+                    //实际读到的字节数
+                    int read = dataInputStream.read(byteArray, 0, byteArray.length);
+                    if(read == -1){
+                        break;
+                    }
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+            }
         }
     }
 }
