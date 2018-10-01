081320eb7057a4aec10a6825765b5fe106f40c9c
lwh <2391519868@qq.com>
2018-10-01 11:31:09 +0800

    Netty学习

--- src/main/java/com/lwh/netty/chp1/demo2/socket/server/MyServerHandler.java
+++ src/main/java/com/lwh/netty/chp1/demo2/socket/server/MyServerHandler.java
@@ -6,6 +6,12 @@ import io.netty.channel.SimpleChannelInboundHandler;
 import java.util.UUID;
 
 public class MyServerHandler extends SimpleChannelInboundHandler<String> {
+    /**
+     * 如果这里面的方法执行时间比较长,会阻塞IO线程池里面的线程,应当由业务线程池来执行任务
+     * @param ctx
+     * @param msg
+     * @throws Exception
+     */
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
         //获取远程的地址和内容
