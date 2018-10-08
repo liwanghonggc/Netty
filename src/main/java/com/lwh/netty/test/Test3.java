6d3c792f7d4e731be8411479731828f55739ee3d
lwh <2391519868@qq.com>
2018-10-08 15:48:16 +0800

    Netty学习

--- src/main/java/com/lwh/netty/test/Test3.java
+++ src/main/java/com/lwh/netty/test/Test3.java
@@ -1,15 +1,24 @@
 package com.lwh.netty.test;
 
-import io.netty.channel.socket.ServerSocketChannel;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+
+import java.nio.charset.Charset;
 
 /**
  * @author lwh
  * @date 2018-10-08
- * @desp channel的parent方法
+ * @desp ByteBuf的toString方法
  */
 public class Test3 {
 
     public static void main(String[] args) {
+        ByteBuf buffer = Unpooled.copiedBuffer("lwh sayHello", Charset.forName("utf-8"));
 
+        //UTF-8
+        System.out.println(Charset.defaultCharset());
+
+        System.out.println(buffer.toString(Charset.forName("utf-8")));
     }
 }
